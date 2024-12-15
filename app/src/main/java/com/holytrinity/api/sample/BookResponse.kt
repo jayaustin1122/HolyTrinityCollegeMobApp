2fef6efe69cdda5fbc78a2783e15a36a3e4b6dc1
Jayaustin1122 <bobisjayaustin5@gmail.com>
2024-12-15 09:53:33 +0800

    Added Sample File and Image In Server Testing

---
+++ app/src/main/java/com/holytrinity/api/sample/BookResponse.kt
@@ -0,0 +1,14 @@
+package com.holytrinity.api.sample
+
+data class BooksResponse(
+    val status: String,
+    val books: List<BookResponse>
+)
+
+data class BookResponse(
+    val id: String?,
+    val book_title: String,
+    val cover_image: String,
+    val content: String,
+    val message: String? = null
+)
