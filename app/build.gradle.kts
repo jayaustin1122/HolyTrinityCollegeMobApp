12582b84153e543e81de2773dc3c6c9b69574fa2
Jayaustin1122 <bobisjayaustin5@gmail.com>
2024-12-21 09:33:37 +0800

    Added Thermal Printer Config

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,7 @@ android {
         viewBinding = true
         dataBinding = true
     }
+
 }
 
 dependencies {
@@ -94,7 +95,6 @@ dependencies {
     implementation ("com.github.f0ris.sweetalert:library:1.6.2")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
